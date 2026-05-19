afff3934abbe566d83f0826f5f6845ec2597625b
Douglas Ribeiro <douglasribeiroo2016@gmail.com>
2026-05-19 00:12:44 -0300

    feat: adiciona componente de orveley

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Categories } from "./components/Categories"
+import { Gallery } from "./components/Gallery"
+import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+
+      <main className="py-10">
+        <Hero />
+        <Categories />
+        <Gallery />
+      </main>
+    </>
+  )
+}
+
+export default App
